9abe4da02b9870b7bf8b051844a30574a56bf1e4
alexandersato0830 <alexandersato0830@gmail.com>
2020-07-09 21:47:09 -0500

    fixed ignorelist

--- build/static/js/13.b71319c2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-amplified"]=this["webpackJsonpreact-amplified"]||[]).push([[13],{212:function(e,t,n){"use strict";n.r(t),n.d(t,"amplify_container",(function(){return c}));var i=n(7),c=function(){function e(e){Object(i.k)(this,e)}return e.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("slot",null))},e}();c.style=":host{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center;height:100vh}"}}]);
-//# sourceMappingURL=13.b71319c2.chunk.js.map
